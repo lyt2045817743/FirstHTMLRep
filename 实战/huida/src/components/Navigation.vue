e8015fe35ca718bdb5d1bf37cfdc044ab5ee5c20
lyt2045817743 <2045817743@qq.com>
2020-04-19 20:49:09 +0800

    网站导航栏静态效果及改变的主题色

--- 实战/huida/src/components/Navigation.vue
+++ 实战/huida/src/components/Navigation.vue
@@ -1,28 +1,83 @@
 <!-- 网站上方导航栏组件 -->
 <template>
-    <div>
-        
+    <div class="main clearfix">
+        <div class="logo">
+            <img src="../assets/img/logo.jpg" alt="">
+        </div>
+        <Menu class="main_nav" mode="horizontal" :theme="theme1" active-name="1">
+            <MenuItem name="1" class="mn_item">
+                <!-- <Icon type="ios-paper" /> -->
+                首页
+            </MenuItem>
+            <MenuItem name="2" class="mn_item">
+                咨询
+            </MenuItem>
+            <MenuItem name="3" class="mn_item">
+                商城
+            </MenuItem>
+            <MenuItem name="4" class="mn_item">
+                论坛
+            </MenuItem>
+            <div class="personal">
+                <MenuItem  v-show="!islogin" name="4">
+                    登录
+                </MenuItem>
+                <img src="../assets/img/head.jpg" v-show="islogin">
+            </div>
+            
+        </Menu>
+        <!-- <br>
+        <p>Change theme</p>
+        <RadioGroup v-model="theme1">
+            <Radio label="light"></Radio>
+            <Radio label="dark"></Radio>
+            <Radio label="primary"></Radio>
+        </RadioGroup> -->
     </div>
 </template>
 
 <script>
-export default {
-data() {
-return {
+    import {Menu,MenuItem} from 'view-design';
+    export default {
+        data() {
+            return {
+                theme1: 'light',
+                islogin: true
+            }
+        },
+        created() {
 
-}
-},
-//生命周期 - 创建完成（访问当前this实例）
-created() {
+        },
+        mounted() {
 
-},
-//生命周期 - 挂载完成（访问DOM元素）
-mounted() {
-
-}
-}
+        },
+        components:{
+            Menu,MenuItem
+        }
+    }
 </script>
 <style scoped>
-/* @import url(); 引入css类 */
-
+    .logo{
+        float: left;
+        height: 60px;
+    }
+    .logo img{
+        width: 100px;
+        height: 60px;
+    }
+    .main_nav{
+        float: right;
+    }
+    .mn_item{
+        margin-right: 40px;
+    }
+    .personal{
+        float: right;
+    }
+    .personal img{
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        margin-top: 5px;
+    }
 </style>
--- 实战/huida/src/main.js
+++ 实战/huida/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'view-design/dist/styles/iview.css'
+import '../my-theme/index.less'
 
 Vue.config.productionTip = false
 
